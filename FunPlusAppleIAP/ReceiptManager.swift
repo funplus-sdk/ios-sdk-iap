f4271455313a1f99dd9a2506003b4d9de92accc1
yuankun.zhang <fmcocoa@gmail.com>
2016-11-14 11:39:05 +0800

    dependecies

--- FunPlusAppleIAP/ReceiptManager.swift
+++ FunPlusAppleIAP/ReceiptManager.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Alamofire
+//import FunPlusSDK
 
 public typealias Receipt = [String: AnyObject]
 
@@ -153,26 +153,27 @@ open class ReceiptManager {
             return
         }
         
-        Alamofire.request(storeRequest as! URLRequestConvertible).responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                guard let receiptInfo = value as? Receipt, let statusCode = receiptInfo["status"] as? Int else {
-                    completion(.failed(error: .responseError))
-                    return
-                }
-                
-                let receiptStatus = ReceiptStatus(rawValue: statusCode) ?? ReceiptStatus.unknown
-                
-                guard receiptStatus.isValid else {
-                    completion(.failed(error: .invalidReceipt(receipt: receiptInfo, status: receiptStatus)))
-                    return
-                }
-                
-                completion(.success(receipt: receiptInfo))
-            case .failure(_):
-                completion(.failed(error: .responseError))
-            }
-        }
+        // TODO
+//        Alamofire.request(storeRequest as! URLRequestConvertible).responseJSON { response in
+//            switch response.result {
+//            case .success(let value):
+//                guard let receiptInfo = value as? Receipt, let statusCode = receiptInfo["status"] as? Int else {
+//                    completion(.failed(error: .responseError))
+//                    return
+//                }
+//                
+//                let receiptStatus = ReceiptStatus(rawValue: statusCode) ?? ReceiptStatus.unknown
+//                
+//                guard receiptStatus.isValid else {
+//                    completion(.failed(error: .invalidReceipt(receipt: receiptInfo, status: receiptStatus)))
+//                    return
+//                }
+//                
+//                completion(.success(receipt: receiptInfo))
+//            case .failure(_):
+//                completion(.failed(error: .responseError))
+//            }
+//        }
     }
     
     class func verifyPurchase(
